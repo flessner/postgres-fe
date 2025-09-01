f862bf468a9a9a2fc28f40992acb2cc6b7b36126
flessner <torben@flessner.dev>
2025-09-01 18:15:06 +0200

    Moved extension commands to entrypoint.sql

--- Dockerfile
+++ Dockerfile
@@ -18,14 +18,6 @@ RUN apt-get update && \
         postgresql-contrib && \
     rm -rf /var/lib/apt/lists/*
 
-COPY nanoid.sql /docker-entrypoint-initdb.d/
-
-RUN echo "CREATE EXTENSION IF NOT EXISTS vector;" > /docker-entrypoint-initdb.d/init-extensions.sql && \
-    echo "CREATE EXTENSION IF NOT EXISTS postgis;" >> /docker-entrypoint-initdb.d/init-extensions.sql && \
-    echo "CREATE EXTENSION IF NOT EXISTS hstore;" >> /docker-entrypoint-initdb.d/init-extensions.sql && \
-    echo "CREATE EXTENSION IF NOT EXISTS pg_cron;" >> /docker-entrypoint-initdb.d/init-extensions.sql && \
-    echo "CREATE EXTENSION IF NOT EXISTS pgcrypto;" >> /docker-entrypoint-initdb.d/init-extensions.sql && \
-    echo "CREATE EXTENSION IF NOT EXISTS pg_stat_statements;" >> /docker-entrypoint-initdb.d/init-extensions.sql && \
-    echo "CREATE EXTENSION IF NOT EXISTS pg_trgm;" >> /docker-entrypoint-initdb.d/init-extensions.sql
+COPY entrypoint.sql /docker-entrypoint-initdb.d/
 
 EXPOSE 5432
--- entrypoint.sql
+++ entrypoint.sql
@@ -1,27 +1,14 @@
--- SOURCE: https://github.com/viascom/nanoid-postgres
-
-/*
- * Copyright 2025 Viascom Ltd liab. Co
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
+-- DEFAULT EXTENSIONS
+CREATE EXTENSION IF NOT EXISTS vector;
+CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS hstore;
+CREATE EXTENSION IF NOT EXISTS pg_cron;
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
+-- NANOID POSTGRES
+-- https://github.com/viascom/nanoid-postgres
 
 -- The `nanoid()` function generates a compact, URL-friendly unique identifier.
 -- Based on the given size and alphabet, it creates a randomized string that's ideal for
